1b4574c46e488cbf2e47c71715e1a756e10d6c73
MC51 (Michael) <mc51@users.noreply.github.com>
2021-05-13 11:05:12 +0200

    remove cocoa related goey files

--- goey/internal/cocoa/debug.m
+++
@@ -1,8 +0,0 @@
-#include "cocoa.h"
-#import <Foundation/NSThread.h>
-#include <stdio.h>
-
-void trace( char const* func ) {
-	printf( "%s\t%p\n", func, [NSThread currentThread] );
-	fflush( stdout );
-}
--- goey/internal/cocoa/doc.go
+++
@@ -1,3 +0,0 @@
-// Package cocoa provides platform-dependent routines required to support the
-// package goey when targeting Cocoa or GNUstep.
-package cocoa
